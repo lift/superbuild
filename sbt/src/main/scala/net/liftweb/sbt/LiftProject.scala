305fc141739b8438d64c0365c96acfb35e22ed33
Indrajit Raychaudhuri <irc@indrajit.com>
2011-03-22 14:23:04 +0530

    Remove access restriction on LiftScalaProject and LiftWebScalaProject

--- sbt/src/main/scala/net/liftweb/sbt/LiftProject.scala
+++ sbt/src/main/scala/net/liftweb/sbt/LiftProject.scala
@@ -113,7 +113,7 @@ trait LiftDefaultDocProject extends DefaultProject with LiftScalaProject {
 }
 
 
-protected trait LiftWebScalaProject extends BasicWebScalaProject with LiftScalaProject {
+trait LiftWebScalaProject extends BasicWebScalaProject with LiftScalaProject {
 
   // Initialize Boot by default
   override def consoleInit =
@@ -129,7 +129,7 @@ protected trait LiftWebScalaProject extends BasicWebScalaProject with LiftScalaP
 }
 
 
-protected trait LiftScalaProject extends BasicScalaProject with Credential with Dependency with Publishing {
+trait LiftScalaProject extends BasicScalaProject with Credential with Dependency with Publishing {
 
   // Auxillary artifacts
   // -------------------
